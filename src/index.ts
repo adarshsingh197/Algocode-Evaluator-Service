ab799b984f60c8503239245ea3bbc97ea53ab311
Adarsh Singh <adarshsingh@Adarshs-MacBook-Pro.local>
2025-01-01 14:05:41 +0530

    BASIC TS configuraton

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express"
+import serverConfig from "./config/serverConfig";
+
+const app = express();
+
+app.listen(serverConfig.PORT,()=>{
+    console.log("hello world")
+    console.log("new hello world")
+})
